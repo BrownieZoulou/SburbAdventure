310dad40fb163d5098157554bcfdb4ba555c10c2
BroZ <ludovicwarin53@gmail.com>
2020-02-06 16:04:44 +0100

    Create Element Display

---
+++ Nodes/AddElementPanel.gd
@@ -0,0 +1,75 @@
+extends Control
+
+var is_it_kid
+
+var new_id
+var new_entity_name
+var new_profile_pic_route
+var new_profile_pic
+var new_hp
+var new_max_hp
+var new_attack
+var new_armor
+var new_weapon_id
+var new_weapon_name
+var new_weapon_kind
+var new_weapon_profile_pic
+var new_weapon_attack
+var new_strenght
+var new_perception
+var new_agility
+var new_charisma
+var new_luck
+var new_strife
+var new_exp_to_give
+var new_gritz
+
+var new_level
+var new_experience
+var new_next_level_experience_needed
+var new_echeladder
+
+func open(isk) :
+	self.visible = true
+	is_it_kid = isk
+	if(is_it_kid) :
+		$KidValue.visible = true
+	else :
+		$KidValue.visible = false
+	
+
+func hide():
+	self.visible = false
+
+
+func _on_CreateElement_button_down():
+	var success = true
+	var error = ""
+	
+	for one_child in self.get_children() :
+		if(one_child.is_in_group("entityValue") || one_child.is_in_group("kidValue") && is_it_kid) :
+			for one_value in one_child.get_children() :
+				var one_text_edit = one_value.get_child(1)
+				if(one_text_edit.get_text() == "" || one_text_edit.get_text() == null) :
+					success = false
+					error += String(one_text_edit.get_parent().get("name")) + " value not Set ! **"
+				else :
+					var value_path
+					value_path = "new_"+String(one_value.name)
+					if(one_value.is_in_group("string")) :
+						self.set(value_path, one_text_edit.text)
+					else :
+						if(!one_text_edit.text.is_valid_integer()):
+							error += one_text_edit.get_parent().name +" value is wrong ! Set an Int !** "
+							success = false
+						else : 
+							self.set(value_path, int(one_text_edit.text))
+	
+	if (success) :
+		hide()
+		save_entity()
+	else:
+		$ErrorDisplayer.set_text(error)
+
+func save_entity() :
+	print("success!!!")
